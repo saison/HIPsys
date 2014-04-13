3f985fd5db6d22282a295741f481490675b73b76
Takuma Nishimura <tak.nishimura.0723@gmail.com>
2014-04-13 16:58:16 +0900

    markup
    sass
    module
    .gitignore
    
    index.phpには何も書いてないのであしからず
    基本的にmodule/header.phpとmodule/footer.phpを読み込んでください。
    基本的なレイアウトはkadai.phpを見てね。
    
    後々調整します。

---
+++ module/footer.php
@@ -0,0 +1,4 @@
+</div><!-- /#wrapper -->
+<footer>Copyright &copy; 2014 <a href="http://www.team-paloc.com" target="_blank">team-paloc</a> All Rights Reserved.</footer>
+</body>
+</html>
---
+++ module/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="css/style.css" media="all">
+  <title>Hal It Portal</title>
+</head>
+<body>
+<header>
+  <div id="logo"><img src="img/hip_logo.png" alt="logo" width="533" height="78"></div>
+</header>
+
+<nav id="gnavi">
+  <ul>
+    <li><a href="kadai.php">課題</a></li>
+    <li><a href="recruit.php">就活</a></li>
+    <li><a href="#">座席</a></li>
+  </ul>
+</nav>
+
+<div id="wrapper">
+
